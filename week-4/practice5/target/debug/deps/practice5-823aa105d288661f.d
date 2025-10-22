40b7ba2d34b88a2f7f6a39f595471f75c0e60e1e
shawn enboy <shawn.enwezoh@pau.edu.ng>
2025-10-22 17:29:14 +0100

    (practice5)

---
+++ week-4/practice5/target/debug/deps/practice5-823aa105d288661f.d
@@ -0,0 +1,5 @@
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-4\practice5\target\debug\deps\practice5-823aa105d288661f.d: src\main.rs
+
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-4\practice5\target\debug\deps\libpractice5-823aa105d288661f.rmeta: src\main.rs
+
+src\main.rs:
