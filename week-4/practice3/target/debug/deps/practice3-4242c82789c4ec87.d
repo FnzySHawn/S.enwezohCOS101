29f906d1f63a23d86ab87d26e2d8c599303bc8bb
shawn enboy <shawn.enwezoh@pau.edu.ng>
2025-10-22 17:08:39 +0100

    (practice3)

---
+++ week-4/practice3/target/debug/deps/practice3-4242c82789c4ec87.d
@@ -0,0 +1,5 @@
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-4\practice3\target\debug\deps\practice3-4242c82789c4ec87.d: src\main.rs
+
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-4\practice3\target\debug\deps\libpractice3-4242c82789c4ec87.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice3/target/debug/deps/practice3.d
@@ -0,0 +1,5 @@
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-4\practice3\target\debug\deps\practice3.d: src\main.rs
+
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-4\practice3\target\debug\deps\practice3.exe: src\main.rs
+
+src\main.rs:
