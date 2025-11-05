d263d76dcbd4b74b2bd87686cde4f98c15e8a3b6
shawn enboy <shawn.enwezoh@pau.edu.ng>
2025-11-05 16:50:05 +0100

    (to string function)

---
+++ week-6/practice6/target/debug/deps/practice6.d
@@ -0,0 +1,5 @@
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-6\practice6\target\debug\deps\practice6.d: src\main.rs
+
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-6\practice6\target\debug\deps\practice6.exe: src\main.rs
+
+src\main.rs:
---
+++ week-6/practice6/target/debug/practice6.d
@@ -0,0 +1,1 @@
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-6\practice6\target\debug\practice6.exe: C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-6\practice6\src\main.rs
