a57813fe721299749141279764ac895712975f2a
shawn enboy <shawn.enwezoh@pau.edu.ng>
2025-11-05 16:21:47 +0100

    (len function)

---
+++ week-6/practice2/target/debug/deps/practice2.d
@@ -0,0 +1,5 @@
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-6\practice2\target\debug\deps\practice2.d: src\main.rs
+
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-6\practice2\target\debug\deps\practice2.exe: src\main.rs
+
+src\main.rs:
---
+++ week-6/practice2/target/debug/practice2.d
@@ -0,0 +1,1 @@
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-6\practice2\target\debug\practice2.exe: C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week-6\practice2\src\main.rs
