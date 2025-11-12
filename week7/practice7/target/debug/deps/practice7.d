2e3b0ca4c108247ad21cd5b5daa7c3460d8d9f2e
shawn enboy <shawn.enwezoh@pau.edu.ng>
2025-11-12 21:45:36 +0100

    (declaring arryas)

---
+++ week7/practice7/target/debug/deps/practice7.d
@@ -0,0 +1,5 @@
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week7\practice7\target\debug\deps\practice7.d: src\main.rs
+
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week7\practice7\target\debug\deps\practice7.exe: src\main.rs
+
+src\main.rs:
---
+++ week7/practice7/target/debug/practice7.d
@@ -0,0 +1,1 @@
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week7\practice7\target\debug\practice7.exe: C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week7\practice7\src\main.rs
