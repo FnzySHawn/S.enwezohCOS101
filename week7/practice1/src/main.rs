f38a0ca18e91a6b2cbb8312564887d80768ad9b3
shawn enboy <shawn.enwezoh@pau.edu.ng>
2025-11-12 16:25:23 +0100

    (invoking a fucntion)

---
+++ week7/practice1/src/main.rs
@@ -0,0 +1,11 @@
+fn my_grade(){
+
+    println!("greetings from function my_grade");
+
+}
+
+fn main() {
+    my_grade();
+
+}
+
---
+++ week7/practice1/target/debug/deps/practice1-b3b73d479043b585.d
@@ -0,0 +1,5 @@
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week7\practice1\target\debug\deps\practice1-b3b73d479043b585.d: src\main.rs
+
+C:\Users\kidme\OneDrive\Documents\S.enwezohCOS101\week7\practice1\target\debug\deps\libpractice1-b3b73d479043b585.rmeta: src\main.rs
+
+src\main.rs:
